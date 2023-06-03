f7ef317bfd485d7a8655e2d8df19e387340b6fb2
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-03 19:38:38 -0300

    chore: configure jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+exports.module = {
+  roots: [
+    '<rootDir>/src'
+  ],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+}
