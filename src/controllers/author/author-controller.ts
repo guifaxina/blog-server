2f081e50987dd045c126c5d1f8ec05f3637785c8
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-09 12:26:49 -0300

    chore: create author controller

---
+++ src/controllers/author/author-controller.ts
@@ -0,0 +1,16 @@
+import { type Author } from '@prisma/client'
+import { type IAuthor } from '../../interfaces/author-interface'
+import { type IAuthorService } from '../../services/author/author-interface'
+import { AuthorService } from '../../services/author/author-service'
+
+export class AuthorController {
+  private readonly authorService: IAuthorService
+
+  constructor () {
+    this.authorService = new AuthorService()
+  }
+
+  public createAuthor = async (author: IAuthor): Promise<Error | Author> => {
+    return await this.authorService.createAuthor(author)
+  }
+}
