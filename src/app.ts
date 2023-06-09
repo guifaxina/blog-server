24044ad426aad9c6b54d48ba2ad68d3c65463a05
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-09 12:19:36 -0300

    chore: create server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-console.log('oi')
+import pino from 'pino'
+import helmet from 'helmet'
+import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { typeDefs } from './api/schema'
+import { Query } from './api/resolvers/query'
+import { Mutation } from './api/resolvers/mutation'
+
+const app = express()
+
+export const logger = pino()
+
+app.use(helmet())
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Mutation
+  }
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 3000 }
+})
+
+logger.info(`Server ready at: ${url}`)
