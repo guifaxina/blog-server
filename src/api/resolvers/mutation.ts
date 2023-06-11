5a0b9afe3ad2706524fee60390e2ba3de80570c1
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-11 09:47:36 -0300

    chore: change implementation of authorcontroller

--- src/api/resolvers/mutation.ts
+++ src/api/resolvers/mutation.ts
@@ -1,6 +1,6 @@
 import { type Author } from '@prisma/client'
 import { type IAuthor } from '../../interfaces/author-interface'
-import { AuthorController } from '../../controllers/author/author-controller'
+import AuthorController from '../../controllers/author/author-controller'
 
 export const Mutation = {
   createAuthor: async (_: any, { input }: any, __: any): Promise<Error | Author> => {
@@ -12,8 +12,7 @@ export const Mutation = {
       password,
       profilePic
     }
-    const controller = new AuthorController()
 
-    return await controller.createAuthor(author)
+    return await AuthorController.createAuthor(author)
   }
 }
