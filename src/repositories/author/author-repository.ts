1f6daed811965172c746bec55753f2afa77ccbb9
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-12 13:27:24 -0300

    chore: change class to default export

--- src/repositories/author/author-repository.ts
+++ src/repositories/author/author-repository.ts
@@ -3,7 +3,7 @@ import { type IAuthor } from '../../interfaces/author-interface'
 import { logger } from '../../app'
 import { type Author } from '@prisma/client'
 
-export class AuthorRepository {
+export default class AuthorRepository {
   public createAuthor = async (data: IAuthor): Promise<Error | Author> => {
     try {
       return await prisma.author.create({
