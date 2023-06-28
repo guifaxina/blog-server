d074d9a9bf7efe64a79a3c74e2b02800f930bb5d
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-28 16:07:50 -0300

    chore: remove test query method

--- src/api/resolvers/query.ts
+++ src/api/resolvers/query.ts
@@ -1,6 +1,5 @@
 import AuthorController from '../../controllers/author/author-controller'
 
 export const Query = {
-  getAuthor: async (_: any, { id }: any, __: any) => await AuthorController.findAuthorById(id),
-  hello: () => 'World'
+  getAuthor: async (_: any, { id }: any, __: any) => await AuthorController.getAuthorById(id)
 }
