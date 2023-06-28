946a9e9e9be95a67492aa74b442a2e74c82e76fd
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-28 16:07:05 -0300

    chore: create post schema

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -1,19 +1,22 @@
 export const typeDefs = `#graphql
   type Query {
     getAuthor(id: ID!): Author!
-    hello: String!
   }
 
   type Mutation {
-    createAuthor(input: CreateAuthorInput!): Author!
+    createAuthor(input: CreateAuthorInput!): Author
+    createPost(input: CreatePostInput!): Post
   }
 
   type Author {
+    id: ID!
     name: String!
     lastName: String!
     email: String!
     password: String!
     profilePic: String!
+    isAccountActive: Boolean!
+    activationCode: String
   }
 
   input CreateAuthorInput {
@@ -22,5 +25,25 @@ export const typeDefs = `#graphql
     email: String!
     password: String!
     profilePic: String!
+  }
+
+  type Post {
+    id: ID!
+    authorId: String!
+    title: String!
+    description: String!
+    readTime: Int!
+    date: Date!
+    tag: String!
+    thumbnail: String!
+    altText: String!
+  }
+
+  type CreatePostInput {
+    title: String!
+    description: String!
+    tag: String!
+    thumbnail: String!
+    altText: String!
   }
 `
