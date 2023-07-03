2b53cc27e3f1ac96d227c8783e718137ed1ffb64
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-02 23:02:27 -0300

    refactor: add correctly typing

--- src/api/resolvers/mutation.ts
+++ src/api/resolvers/mutation.ts
@@ -1,18 +1,14 @@
-import { type Author } from '@prisma/client'
+import { type Author, type Post } from '@prisma/client'
 import { type IAuthor } from '../../interfaces/author-interface'
+import { type IPost } from '../../interfaces/post-interface'
 import AuthorController from '../../controllers/author/author-controller'
+import PostController from '../../controllers/post/post-controller'
 
 export const Mutation = {
-  createAuthor: async (_: any, { input }: any, __: any): Promise<Error | Author> => {
-    const { name, lastName, email, password, profilePic }: IAuthor = input
-    const author = {
-      name,
-      lastName,
-      email,
-      password,
-      profilePic
-    }
-
-    return await AuthorController.createAuthor(author)
+  createAuthor: async (_: any, args: { input: IAuthor }): Promise<Error | Author> => {
+    return await AuthorController.createAuthor(args.input)
+  },
+  createPost: async (_: any, args: { input: IPost }): Promise<Error | Post> => {
+    return await PostController.createPost(args.input)
   }
 }
--- src/api/resolvers/query.ts
+++ src/api/resolvers/query.ts
@@ -1,5 +1,5 @@
 import AuthorController from '../../controllers/author/author-controller'
 
 export const Query = {
-  getAuthor: async (_: any, { id }: any, __: any) => await AuthorController.getAuthorById(id)
+  getAuthor: async (_: any, args: { id: string }) => await AuthorController.getAuthorById(args.id)
 }
