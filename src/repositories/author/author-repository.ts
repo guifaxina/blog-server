65dadf392a520814c6fe58a5eeb4f63ead86686c
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-02 23:24:37 -0300

    chore: handle unique constraint violation in author creat ion

--- src/repositories/author/author-repository.ts
+++ src/repositories/author/author-repository.ts
@@ -1,7 +1,8 @@
 import prisma from '../../../prisma/database'
-import { type IAuthor } from '../../interfaces/author-interface'
 import { logger } from '../../app'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 import { type Author } from '@prisma/client'
+import { type IAuthor } from '../../interfaces/author-interface'
 
 export default class AuthorRepository {
   public createAuthor = async (data: IAuthor): Promise<Error | Author> => {
@@ -10,6 +11,9 @@ export default class AuthorRepository {
         data
       })
     } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
+        throw new Error('Email already in use.')
+      }
       logger.error(error)
       throw new Error('An unexpected error occurred.')
     }
