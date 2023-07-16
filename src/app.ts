3eb967fab80e9b6e4671ab9ab1253e5857cb2b15
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-16 17:52:47 -0300

    refactor: change server initializing to make possible the use of middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,42 @@
 import pino from 'pino'
-import helmet from 'helmet'
+import cors from 'cors'
 import express from 'express'
+import http from 'http'
+import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 import { typeDefs } from './api/schema'
 import { Query } from './api/resolvers/query'
 import { Mutation } from './api/resolvers/mutation'
+
+interface IContext {
+  req: express.Request
+}
 
 const app = express()
 
 export const logger = pino()
 
-app.use(helmet())
+const httpServer = http.createServer(app)
 
-const server = new ApolloServer({
+const server = new ApolloServer<IContext>({
   typeDefs,
   resolvers: {
     Query,
     Mutation
-  }
+  },
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
 
 async function startServer (): Promise<void> {
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 3000 }
-  })
-  logger.info(`Server ready at: ${url}`)
+  await server.start()
+
+  app.use('/graphql', cors(), express.json(), express.urlencoded({ extended: true }), expressMiddleware(server, {
+    context: async ({ req }) => ({ req })
+  }))
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+  console.log('Server ready at: http://localhost:4000/graphql')
 }
 
 void startServer()
