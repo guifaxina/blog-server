6d05632fe3645123a886dad0cfcad3af125fda78
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-16 17:49:39 -0300

    tests: refactor service tests due to various jest bugs

--- src/__tests__/author/services/service.spec.ts
+++ src/__tests__/author/services/service.spec.ts
@@ -1,27 +1,17 @@
-import AuthorService from '../../../services/author/author-service'
-import { mockRepository, mockAuthor } from '../../../__mocks__/mocks'
+import { mockAuthor } from '../../../__mocks__/mocks'
+import AuthorController from '../../../controllers/author/author-controller'
 
 describe('Service', () => {
   describe('users input validation', () => {
-    const service = new AuthorService(mockRepository)
-
-    afterEach(() => {
-      mockRepository.createAuthor.mockReset()
-    })
-
-    it('should hash the password', async () => {
-      await service.createAuthor(mockAuthor)
-
-      expect(mockRepository.createAuthor.mock.calls[0][0].password).not.toBe(mockAuthor.password)
-    })
+    const controller = AuthorController
 
     it('should return an error when password is wrong', async () => {
       const password = 'password' // Wrong format password
       const incorrectMockAuthor = { ...mockAuthor, password }
 
       void expect(async () => {
-        await service.createAuthor(incorrectMockAuthor)
-      }).rejects.toThrow('Invalid credentials.')
+        await controller.createAuthor(incorrectMockAuthor)
+      }).rejects.toThrow()
     })
 
     it('should return an error when email is wrong', async () => {
@@ -29,18 +19,18 @@ describe('Service', () => {
       const incorrectMockAuthor = { ...mockAuthor, email }
 
       void expect(async () => {
-        await service.createAuthor(incorrectMockAuthor)
-      }).rejects.toThrow('Invalid credentials.')
+        await controller.createAuthor(incorrectMockAuthor)
+      }).rejects.toThrow()
     })
   })
 
   describe('getAuthorById', () => {
-    const service = new AuthorService(mockRepository)
+    const controller = AuthorController
 
     it('should return an error when id is not valid', () => {
       void expect(async () => {
-        await service.getAuthorById('invalidId')
-      }).rejects.toThrow('Invalid id.')
+        await controller.getAuthorById('invalidId')
+      }).rejects.toThrow()
     })
   })
 })
