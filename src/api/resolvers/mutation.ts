13afd69830430cd25c259fe8966d25b4edfa473d
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-27 15:00:49 -0300

    feat: create session when users create an account

--- src/api/resolvers/mutation.ts
+++ src/api/resolvers/mutation.ts
@@ -4,9 +4,17 @@ import { type IPost } from '../../interfaces/post-interface'
 import AuthorController from '../../controllers/author/author-controller'
 import PostController from '../../controllers/post/post-controller'
 
+interface IContext {
+  req: Express.Request
+}
+
 export const Mutation = {
-  createAuthor: async (_: any, args: { input: IAuthor }): Promise<Error | Author> => {
-    return await AuthorController.createAuthor(args.input)
+  createAuthor: async (_: any, args: { input: IAuthor }, context: IContext): Promise<Author> => {
+    const author: Author = await AuthorController.createAuthor(args.input)
+
+    context.req.session.userId = author.id
+
+    return author
   },
   createPost: async (_: any, args: { input: IPost }): Promise<Error | Post> => {
     return await PostController.createPost(args.input)
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import pino from 'pino'
 import cors from 'cors'
 import express from 'express'
 import http from 'http'
+import session from 'express-session'
+import RedisStore from 'connect-redis'
+import { redis } from './utils/redis.config'
 import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServer } from '@apollo/server'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
@@ -9,9 +12,17 @@ import { typeDefs } from './api/schema'
 import { Query } from './api/resolvers/query'
 import { Mutation } from './api/resolvers/mutation'
 
+declare module 'express-session' {
+  export interface SessionData {
+    userId: string
+  }
+}
+
 interface IContext {
   req: express.Request
 }
+
+const monthDuration = 1000 * 60 * 60 * 24 * 30
 
 const app = express()
 
@@ -28,15 +39,27 @@ const server = new ApolloServer<IContext>({
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
 
+const redisStore = new RedisStore({
+  client: redis
+})
+
 async function startServer (): Promise<void> {
   await server.start()
 
-  app.use('/graphql', cors(), express.json(), express.urlencoded({ extended: true }), expressMiddleware(server, {
+  app.use('/graphql', cors(), express.json(), express.urlencoded({ extended: true }), session({
+    store: redisStore,
+    secret: process.env.SESSION_SECRET as string,
+    cookie: {
+      maxAge: monthDuration
+    }
+  }), expressMiddleware(server, {
     context: async ({ req }) => ({ req })
   }))
 
   await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
   console.log('Server ready at: http://localhost:4000/graphql')
+  await redis.connect()
+  console.log('Redis connected')
 }
 
 void startServer()
